d425277053704a05d5f0c25ee1eaaa9b5d6294f2
Jim Lombardo <james.g.lombardo@gmail.com>
2015-11-09 14:38:02 -0600

    maintenance commit

---
+++ src/main/java/edu/wctc/samples/jpahotel/repository/HotelRepository.java
@@ -0,0 +1,27 @@
+package edu.wctc.samples.jpahotel.repository;
+
+import edu.wctc.samples.jpahotel.entity.Hotel;
+import java.io.Serializable;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * Spring-data-jpa will automatically create a proxy object from this
+ * interface contract that contains all basic C.R.U.D. functions
+ * 
+ * @author jlombardo
+ */
+public interface HotelRepository extends JpaRepository<Hotel, Integer>, Serializable {
+    
+    // basic C.R.U.D. queries auto generated and not visible here
+    
+    // these custom queries added by developer
+    
+    @Query("delete from Hotel h where h.hotelId = :id")
+    void deleteById(@Param("id") Integer id);
+    
+    @Query("select h from Hotel h where h.name LIKE :searchKey OR h.city LIKE :searchKey OR h.zip LIKE :searchKey")
+    List<Hotel> searchForHotelByAny(@Param("searchKey") String searchKey);
+}
